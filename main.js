5f97cfce73424584907b96d7c20e02ffd06acfe2
Dominic <dominicduf@gmail.com>
2025-04-29 15:11:30 -0400

    Create the 16x16 square grid
    Use flex to create rows and columns. To make sure
    they are squares, use the aspect-ratio: 1/1.

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 // main container
-const container = document.createElement("div");
-container.id = "main-div"
-document.body.appendChild(container);
-
-
+const container = document.getElementById("main-div");
 for (i=0; i<16; i++) {
-    const square_div = document.createElement("div");
-    square_div.className = "square-div"
-    container.appendChild(square_div);
+
+    const row_div = document.createElement("div");
+    row_div.className = "row-div"
+    container.appendChild(row_div);
+
+    for (j=0; j<16; j++) {
+        const square_div = document.createElement("div");
+        square_div.className = "square-div"
+        row_div.appendChild(square_div);
+    }
+
 }
